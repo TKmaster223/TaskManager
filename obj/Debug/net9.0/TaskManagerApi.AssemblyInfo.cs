a10e4467a8277215dcf78eabf6ec08cb1687017e
Farouk Musa <farouk7159@bazeuniversity.edu.ng>
2024-12-21 01:23:52 +0100

    First hopefully the st

--- obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72018738039cf064d2201863e7f21862faee750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5931b23762d48a3867d09a7f3827c7a8aa44ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
