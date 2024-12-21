e68e3347bcecbd162975b60c23c757df39f8ff9f
Farouk Musa <farouk7159@bazeuniversity.edu.ng>
2024-12-21 19:01:00 +0100

    ADDED DELETE ENDPOINT

--- obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5931b23762d48a3867d09a7f3827c7a8aa44ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10e4467a8277215dcf78eabf6ec08cb1687017e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
